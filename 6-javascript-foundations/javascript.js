e8e8541a1e36081e012581511e6d8195c28184c8
derektg <121468325+dertan1170@users.noreply.github.com>
2024-11-11 10:34:17 -0800

    Create javascript.js

---
+++ 6-javascript-foundations/javascript.js
@@ -0,0 +1,34 @@
+// console.log("Hello, World!")
+
+// let name = "John";
+// let surname = "Doe";
+// console.log(name);
+// console.log(surname);
+
+// let age = 11;
+// console.log(age);
+// age = 54;
+// console.log(age);
+
+// const pi = 3.14;
+// // pi = 10;
+// console.log(pi);
+
+// // #1
+// console.log(23 + 97);
+// // #2
+// console.log(1+2+3+4+5+6);
+// // #3
+// console.log((4 + 6 + 9) / 77);
+// // #4
+// let a = 10;
+// console.log(a);
+// console.log(9 * a);
+// let b = 7 * a;
+// console.log(b);
+// #5
+let max = 57;
+let actual = max - 13;
+let percentage = actual / max;
+console.log(percentage);
+
